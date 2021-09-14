805f01aadc3a5224d1e62f43fbfcf0862e1974eb
James Yang <26634873@qq.com>
2021-09-14 07:46:34 +0000

    1.0.2

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ export env.json key/value as bash shell exports
 ## Usage
 
 ```sh
-npx env2sh > env.sh
+npx env2sh env.json > env.sh
 . env.sh
 ```
 
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
+const path = require('path')
+const fs = require('fs')
 // usage: npx envexport > env.sh
 function main() {
-    const env = require(process.argv[2] || "./env.json")
-    const sh = Object.entries(env).map(([a, b]) => "export " + a + "=" + JSON.stringify(b)).join("\n")
-    console.log(sh)
+    try {
+        const env = JSON.parse(fs.readFileSync(process.argv[2] || "env.json", 'utf8'))
+        const sh = Object.entries(env).map(([a, b]) => "export " + a + "=" + JSON.stringify(b)).join("\n")
+        console.log(sh)
+    } catch(e) {
+        console.error(e.message)
+        process.exit(1)
+    }
 }
 
 module.exports = main
